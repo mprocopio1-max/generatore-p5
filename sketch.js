43eb862841db4dfa6d4ef96efe58406a71295968
mprocopio1-max <m.procopio1@ied.edu>
2026-03-31 10:06:51 +0200

    isola base

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
 function setup() {
-  createCanvas(400, 400);
-}
+  createCanvas(200, 200);
 
-function draw() {
-  background(220);
+  background(0);
+  noStroke();
+
+  let centralx = width / 2;
+  let centraly = height / 2;
+
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+  
+      console.log (x, y);
+      
+      //distanza dal centro
+      let distanceFromCenter = dist(x, y, centralx, centraly);
+      let normdistanceFromCenter = distanceFromCenter / (width/2);
+      
+      let atitude = 1 - normdistanceFromCenter;
+
+      fill(atitude * 255);
+      rect(x, y, 1, 1);
+      
+    }
+ }
 }
