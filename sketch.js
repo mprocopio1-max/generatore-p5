c36c0e942291a333589522988580bc469698f850
mprocopio1-max <m.procopio1@ied.edu>
2026-03-31 12:30:50 +0200

    sprites

--- sketch.js
+++ sketch.js
@@ -1,15 +1,21 @@
 let perlinScale = 200;
-let tile_size = 5;
+let tile_size = 20;
+let sprites_size = 25;
 
 let grassImage; 
 let sandImage;
 let waterImage;
+let flowerImage;
+let mushroomImage;
 
 function preload() {
   //carica risorse
-  grassImage = loadImage('tiles/grass.png');
-  sandImage = loadImage('tiles/sand.png');
-  waterImage = loadImage('tiles/water.png');  
+  grassImage = loadImage('assets/tiles/grass.png');
+  sandImage = loadImage('assets/tiles/sand.png');
+  waterImage = loadImage('assets/tiles/water.png');
+  
+  flowerImage = loadImage('assets/sprites/flower.png');
+  mushroomImage = loadImage('assets/sprites/mushroom.png');
 }
 
 function setup() {
@@ -39,9 +45,11 @@ function setup() {
       altitude+= perlin;
       altitude-= 0.5;
 
-      //assegna colore
+      //tiles
       let seaLevel = 0.2;
       let beachLevel = 0.25;
+
+      let img;
       if (altitude < seaLevel) {
         img=waterImage;
       } else if (altitude < beachLevel) { 
@@ -49,8 +57,38 @@ function setup() {
       } else {
         img=grassImage;
       }
-
+      
       image(img, x, y, tile_size, tile_size);
+ }
+  for (let x = 0; x < width; x = x + tile_size) {
+    for (let y = 0; y < height; y = y + tile_size) {
+  
+      console.log (x, y);
+      
+      //distanza dal centro
+      let distanceFromCenter = dist(x, y, centralx, centraly);
+      let normdistanceFromCenter = distanceFromCenter / (width/2);
+      
+      let altitude = 1 - normdistanceFromCenter;
+
+      //noise 
+      noiseDetail(6);
+      let perlin = noise (x / perlinScale, y / perlinScale);
+      altitude*= perlin;
+      altitude+= perlin;
+      altitude-= 0.5;
+
+       let beachLevel = 0.25;
+      //  Flowers
+       if (random() < 0.01 && altitude > beachLevel) {
+        image(flowerImage, x, y, sprites_size, sprites_size);
+      }
+
+      // Mushrooms
+      if (random() < 0.008 && altitude > beachLevel) {
+        image(mushroomImage, x, y, sprites_size, sprites_size);
+      }
     }
  }
+}
 }
